81700744af2ca0f8e8e8bc644be160f7250b74b8
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-22 13:40:15 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import './css/styles.css';
+import { getRefs } from './js/getRefs.js';
+import { fetchCountries } from './js/fetchCountries';
+import {
+  renderListMarkup,
+  renderCardMarkup,
+  clearForm,
+} from './js/renderMarkup';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const refs = getRefs();
+
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+    let name = e.target.value.trim();
+    if (name = '') {
+        clearForm();
+        // renderCardMarkup(resoult);
+    }
+    else {
+      fetchCountries(name).then(onCheck).catch(onError);
+    }
+}
+
+function onCheck(result) {
+  if (result.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (result.length === 1) {
+    clearForm();
+    renderCardMarkup(result);
+  } else {
+    clearForm();
+    renderListMarkup(result);
+  }
+}
+
+function onError(error) {
+  clearForm();
+  Notify.failure('Oops, something is wrong');
+}
